8794e7ef9ccb7be830e9e8ea9cc27a2b452432b7
MrMrch <85297052+MrMrch@users.noreply.github.com>
2021-09-25 14:00:39 +0200

    Create linked_demo_list.c

---
+++ linked_demo_list.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    
+    node *list = NULL;
+    
+    node *n = malloc(sizeof(node));
+    
+    if(n != NULL)
+    {
+        //(*n).number = 1;
+        n->number = 1;
+    }
+    
+    list = n;
+    
+    node *n = malloc(sizeof(node));
+    if (n != NULL)
+    {
+        
+        n->number = 2;
+        n->next = NULL;
+    }
+    
+    list->next = n;
+    
+    node *n = malloc(sizeof(node));
+    if (n != NULL)
+    {
+        
+        n->number = 3;
+        n->next = NULL;
+    }
+    
+    list->next->next = n;
+}
